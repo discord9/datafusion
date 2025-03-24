5bbedc6704162afb03478f56ffb629405a4e1220
Ruihang Xia <waynestxia@gmail.com>
2025-03-24 10:25:43 -0700

    feat: include some BinaryOperator from sqlparser (#15327)
    * include some BinaryOperator from sqlparser
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * slt for new operators
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * add document about availability
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    ---------
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- datafusion/sql/src/unparser/expr.rs
+++ datafusion/sql/src/unparser/expr.rs
@@ -902,6 +902,19 @@ impl Unparser<'_> {
             BinaryOperator::StringConcat => Ok(Operator::StringConcat),
             BinaryOperator::AtArrow => Ok(Operator::AtArrow),
             BinaryOperator::ArrowAt => Ok(Operator::ArrowAt),
+            BinaryOperator::Arrow => Ok(Operator::Arrow),
+            BinaryOperator::LongArrow => Ok(Operator::LongArrow),
+            BinaryOperator::HashArrow => Ok(Operator::HashArrow),
+            BinaryOperator::HashLongArrow => Ok(Operator::HashLongArrow),
+            BinaryOperator::AtAt => Ok(Operator::AtAt),
+            BinaryOperator::DuckIntegerDivide | BinaryOperator::MyIntegerDivide => {
+                Ok(Operator::IntegerDivide)
+            }
+            BinaryOperator::HashMinus => Ok(Operator::HashMinus),
+            BinaryOperator::AtQuestion => Ok(Operator::AtQuestion),
+            BinaryOperator::Question => Ok(Operator::Question),
+            BinaryOperator::QuestionAnd => Ok(Operator::QuestionAnd),
+            BinaryOperator::QuestionPipe => Ok(Operator::QuestionPipe),
             _ => not_impl_err!("unsupported operation: {op:?}"),
         }
     }
@@ -937,8 +950,19 @@ impl Unparser<'_> {
             Operator::BitwiseShiftRight => Ok(BinaryOperator::PGBitwiseShiftRight),
             Operator::BitwiseShiftLeft => Ok(BinaryOperator::PGBitwiseShiftLeft),
             Operator::StringConcat => Ok(BinaryOperator::StringConcat),
-            Operator::AtArrow => not_impl_err!("unsupported operation: {op:?}"),
-            Operator::ArrowAt => not_impl_err!("unsupported operation: {op:?}"),
+            Operator::AtArrow => Ok(BinaryOperator::AtArrow),
+            Operator::ArrowAt => Ok(BinaryOperator::ArrowAt),
+            Operator::Arrow => Ok(BinaryOperator::Arrow),
+            Operator::LongArrow => Ok(BinaryOperator::LongArrow),
+            Operator::HashArrow => Ok(BinaryOperator::HashArrow),
+            Operator::HashLongArrow => Ok(BinaryOperator::HashLongArrow),
+            Operator::AtAt => Ok(BinaryOperator::AtAt),
+            Operator::IntegerDivide => Ok(BinaryOperator::DuckIntegerDivide),
+            Operator::HashMinus => Ok(BinaryOperator::HashMinus),
+            Operator::AtQuestion => Ok(BinaryOperator::AtQuestion),
+            Operator::Question => Ok(BinaryOperator::Question),
+            Operator::QuestionAnd => Ok(BinaryOperator::QuestionAnd),
+            Operator::QuestionPipe => Ok(BinaryOperator::QuestionPipe),
         }
     }
 
